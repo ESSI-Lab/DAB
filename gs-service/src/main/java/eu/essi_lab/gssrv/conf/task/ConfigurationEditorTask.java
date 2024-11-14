42f8355ac2f120881ec605fa0470df33a96a7895
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2024-11-14 14:59:40 +0100

    [CONFIGURATION-EDITOR-TASK] minor updates

--- gs-service/src/main/java/eu/essi_lab/gssrv/conf/task/ConfigurationEditorTask.java
+++ gs-service/src/main/java/eu/essi_lab/gssrv/conf/task/ConfigurationEditorTask.java
@@ -3,6 +3,27 @@
  */
 package eu.essi_lab.gssrv.conf.task;
 
+/*-
+ * #%L
+ * Discovery and Access Broker (DAB) Community Edition (CE)
+ * %%
+ * Copyright (C) 2021 - 2024 National Research Council of Italy (CNR)/Institute of Atmospheric Pollution Research (IIA)/ESSI-Lab
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -74,7 +95,7 @@ public class ConfigurationEditorTask extends AbstractCustomTask {
 	// scheduling
 	//
 
-	Date startDate = new Date();
+	Date startDate = new Date(TimeUnit.DAYS.toMillis(10));
 
 	Scheduling scheduling = harvestingSetting.getScheduling();
 	scheduling.setEnabled(true);
@@ -85,6 +106,8 @@ public class ConfigurationEditorTask extends AbstractCustomTask {
 	scheduling.setStartTime(startDate);
 
 	scheduler.schedule(harvestingSetting);
+	
+	SelectionUtils.deepClean(harvestingSetting);
 
 	//
 	//
@@ -152,12 +175,6 @@ public class ConfigurationEditorTask extends AbstractCustomTask {
 		wrapper.selectConnectorType(connectorType.get());
 	    }
 	}
-
-	//
-	//
-	//
-
-	SelectionUtils.deepClean(harvSetting);
 
 	//
 	//
