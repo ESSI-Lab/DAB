0cee331032b12cfa6bfda0eb05d3b8773d7367fa
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2024-12-04 14:37:57 +0100

    LegendsViewer class created

---
+++ configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/grid/LegendsViewer.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package eu.essi_lab.cfga.gui.components.grid;
+
+import java.util.List;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+
+/**
+ * @author Fabrizio
+ */
+@SuppressWarnings("serial")
+public class LegendsViewer extends HorizontalLayout {
+
+    /**
+     * 
+     */
+    public LegendsViewer(GridComponent grid, List<Component> legends) {
+
+	setMargin(false);
+	setSpacing(false);
+	setHeight("30px");
+
+	legends.forEach(leg -> add(leg));
+    }
+}
