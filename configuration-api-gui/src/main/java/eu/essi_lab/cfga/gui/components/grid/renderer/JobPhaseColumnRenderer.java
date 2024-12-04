3741005e07395b94b4cef8b0cc4ef2f570e125eb
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2024-12-04 15:23:14 +0100

    JobPhaseColumnRenderer updated with the LegendBuilder

--- configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/grid/renderer/JobPhaseColumnRenderer.java
+++ configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/grid/renderer/JobPhaseColumnRenderer.java
@@ -28,13 +28,10 @@ import java.util.HashMap;
 import java.util.Optional;
 
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import eu.essi_lab.cfga.gui.components.ComponentFactory;
+import eu.essi_lab.cfga.gui.components.grid.LegendBuilder;
 import eu.essi_lab.messages.JobStatus.JobPhase;
 
 /**
@@ -55,130 +52,41 @@ public class JobPhaseColumnRenderer extends IconColumnRenderer {
      * @return
      */
     public Optional<Component> getLegend() {
-    
-        HorizontalLayout layout = ComponentFactory.createNoSpacingNoMarginHorizontalLayout();
-        layout.getStyle().set("padding-top", "3px");
-        layout.getStyle().set("padding-bottom", "3px");
-        layout.getStyle().set("padding-left", "3px");
-        layout.getStyle().set("padding-right", "15px");
-    
-        Label legendLabel = ComponentFactory.createLabel("Status", false, 12);
-        legendLabel.getStyle().set("font-weight", "bold");
-        legendLabel.getStyle().set("margin-right", "-10px");
-        legendLabel.getStyle().set("margin-top", "-2px");
-    
-        layout.add(legendLabel);
-    
-        layout.add(crateLegendPart(JobPhase.RUNNING_LABEL, createIcon(JobPhase.RUNNING_LABEL, true, 3), 50, 5));
-    
-        layout.add(crateLegendPart(JobPhase.CANCELED_LABEL, createIcon(JobPhase.CANCELED_LABEL, true, 5), 50, -8));
-    
-        layout.add(crateLegendPart(JobPhase.COMPLETED_LABEL, createIcon(JobPhase.COMPLETED_LABEL, true, 10), 55, -5));
-    
-        layout.add(crateLegendPart(JobPhase.ERROR_LABEL, createIcon(JobPhase.ERROR_LABEL, true, -5), 30, 0));
-    
-        layout.add(crateLegendPart(JobPhase.RESCHEDULED_LABEL, createIcon(JobPhase.RESCHEDULED_LABEL, true, 10), 60, -5));
-    
-        return Optional.of(layout);
+
+	Component component = LegendBuilder.get().//
+		addLegendPart(JobPhase.RUNNING_LABEL, createIcon(JobPhase.RUNNING_LABEL), 3, 50, 5).//
+		addLegendPart(JobPhase.CANCELED_LABEL, createIcon(JobPhase.CANCELED_LABEL), 5, 50, -8).//
+		addLegendPart(JobPhase.COMPLETED_LABEL, createIcon(JobPhase.COMPLETED_LABEL), 10, 55, -5).//
+		addLegendPart(JobPhase.ERROR_LABEL, createIcon(JobPhase.ERROR_LABEL), -5, 30, 0).//
+		addLegendPart(JobPhase.RESCHEDULED_LABEL, createIcon(JobPhase.RESCHEDULED_LABEL), 10, 60, -5).//
+		build("Status");
+
+	return Optional.of(component);
     }
 
     @Override
     protected Icon createIcon(HashMap<String, String> item) {
-    
-        String status = item.get("Status");
-        return createIcon(status);
+
+	String status = item.get("Status");
+	return createIcon(status);
     }
 
     @Override
     protected Optional<String> getToolTip(HashMap<String, String> item) {
-    
-        String status = item.get("Status");
-        switch (status) {
-    
-        case JobPhase.RUNNING_LABEL:
-        case JobPhase.CANCELED_LABEL:
-        case JobPhase.COMPLETED_LABEL:
-        case JobPhase.ERROR_LABEL:
-        case JobPhase.RESCHEDULED_LABEL:
-    
-            return Optional.of(status);
-        }
-    
-        return Optional.empty();
-    }
 
-    /**
-     * @param name
-     * @param icon
-     * @return
-     */
-    private VerticalLayout crateLegendPart(String name, Icon icon, int width, int marginLeft) {
-
-	VerticalLayout vl = ComponentFactory.createNoSpacingNoMarginVerticalLayout();
-	vl.getStyle().set("width", String.valueOf(width) + "px");
-	vl.getStyle().set("margin-left", marginLeft + "px");
-	vl.add(ComponentFactory.createLabel(name, false, 10));
-	vl.add(icon);
-
-	return vl;
-    }
-
-    /**
-     * @param status
-     * @return
-     */
-    private Icon createIcon(String status, boolean fixedSize, Integer marginLeft) {
-
-	Icon icon = null;
-
+	String status = item.get("Status");
 	switch (status) {
 
 	case JobPhase.RUNNING_LABEL:
-
-	    icon = VaadinIcon.PLAY_CIRCLE_O.create();
-
-	    break;
-
 	case JobPhase.CANCELED_LABEL:
-
-	    icon = VaadinIcon.CLOSE_CIRCLE_O.create();
-
-	    break;
-
 	case JobPhase.COMPLETED_LABEL:
-
-	    icon = VaadinIcon.CHECK_SQUARE_O.create();
-
-	    break;
-
 	case JobPhase.ERROR_LABEL:
-
-	    icon = VaadinIcon.WARNING.create();
-
-	    break;
-
 	case JobPhase.RESCHEDULED_LABEL:
 
-	    icon = VaadinIcon.ALARM.create();
-
-	    break;
-
-	default:
-	    return IconColumnRenderer.getEmptyIcon();
+	    return Optional.of(status);
 	}
 
-	if (fixedSize) {
-
-	    icon.getStyle().set("width", "30px !important");
-	    icon.getStyle().set("height", "15px !important");
-	}
-
-	if (marginLeft != null) {
-
-	    icon.getStyle().set("margin-left", String.valueOf(marginLeft) + "px");
-	}
-
-	return icon;
+	return Optional.empty();
     }
 
     /**
@@ -187,6 +95,30 @@ public class JobPhaseColumnRenderer extends IconColumnRenderer {
      */
     private Icon createIcon(String status) {
 
-	return createIcon(status, false, null);
+	switch (status) {
+
+	case JobPhase.RUNNING_LABEL:
+
+	    return VaadinIcon.PLAY_CIRCLE_O.create();
+
+	case JobPhase.CANCELED_LABEL:
+
+	    return VaadinIcon.CLOSE_CIRCLE_O.create();
+
+	case JobPhase.COMPLETED_LABEL:
+
+	    return VaadinIcon.CHECK_SQUARE_O.create();
+
+	case JobPhase.ERROR_LABEL:
+
+	    return VaadinIcon.WARNING.create();
+
+	case JobPhase.RESCHEDULED_LABEL:
+
+	    return VaadinIcon.ALARM.create();
+
+	default:
+	    return IconColumnRenderer.getEmptyIcon();
+	}
     }
 }
