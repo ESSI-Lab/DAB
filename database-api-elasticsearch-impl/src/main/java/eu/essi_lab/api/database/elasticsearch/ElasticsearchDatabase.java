d39d9db3f3c93095a0d59cae493fa8345eebae08
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-01-06 15:02:14 +0100

    [main-private] license comments updated to year 2025

--- database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/elasticsearch/ElasticsearchDatabase.java
+++ database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/elasticsearch/ElasticsearchDatabase.java
@@ -10,7 +10,7 @@ import java.util.Optional;
  * #%L
  * Discovery and Access Broker (DAB) Community Edition (CE)
  * %%
- * Copyright (C) 2021 - 2024 National Research Council of Italy (CNR)/Institute of Atmospheric Pollution Research (IIA)/ESSI-Lab
+ * Copyright (C) 2021 - 2025 National Research Council of Italy (CNR)/Institute of Atmospheric Pollution Research (IIA)/ESSI-Lab
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
