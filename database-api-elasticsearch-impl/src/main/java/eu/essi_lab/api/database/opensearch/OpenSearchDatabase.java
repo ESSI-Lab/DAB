e6a4455d14f95a04f970282b4757aac88e0893ee
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-01-07 17:36:21 +0100

    [main-private] OpenSearchDatabase test

--- database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/OpenSearchDatabase.java
+++ database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/OpenSearchDatabase.java
@@ -3,8 +3,15 @@
  */
 package eu.essi_lab.api.database.opensearch;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch._types.OpenSearchException;
+import org.opensearch.client.opensearch.core.InfoResponse;
+import org.opensearch.client.transport.aws.AwsSdk2Transport;
+import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
 
 /*-
  * #%L
@@ -30,15 +37,44 @@ import java.util.Optional;
 import eu.essi_lab.api.database.Database;
 import eu.essi_lab.api.database.DatabaseFolder;
 import eu.essi_lab.api.database.SourceStorageWorker;
-import eu.essi_lab.api.database.Database.IdentifierType;
 import eu.essi_lab.cfga.gs.setting.database.DatabaseSetting;
 import eu.essi_lab.model.StorageInfo;
 import eu.essi_lab.model.exceptions.GSException;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
 
 /**
  * @author Fabrizio
  */
 public class OpenSearchDatabase extends Database {
+
+    public static void main(String[] args) throws OpenSearchException, IOException {
+
+	AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+
+	AwsSdk2TransportOptions awsSdk2TransportOptions = AwsSdk2TransportOptions.builder().//
+		setCredentials(credentialsProvider).//
+		build();
+
+	SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+
+	AwsSdk2Transport awsSdk2Transport = new AwsSdk2Transport(//
+		httpClient, //
+		"w6iz16h1nis29el1etjd.us-east-1.aoss.amazonaws.com", //
+		"aoss", // Amazon OpenSearch Serverless
+		Region.US_EAST_1, // signing service region
+		awsSdk2TransportOptions);
+
+	OpenSearchClient client = new OpenSearchClient(awsSdk2Transport);
+
+	InfoResponse info = client.info();
+	System.out.println(info.version().distribution() + ": " + info.version().number());
+
+	httpClient.close();
+    }
 
     @Override
     public boolean supports(StorageInfo dbUri) {
