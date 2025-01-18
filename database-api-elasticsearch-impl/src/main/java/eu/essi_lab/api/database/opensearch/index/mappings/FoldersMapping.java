a16980fd173cc0fdaf566712ca887fa85aeb17cd
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-01-18 14:50:47 +0100

    [GIP-753] creted FoldersMapping

---
+++ database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/index/mappings/FoldersMapping.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package eu.essi_lab.api.database.opensearch.index.mappings;
+
+/**
+ * @author Fabrizio
+ */
+public class FoldersMapping extends IndexMapping {
+
+    /**
+     * 
+     */
+    public static final String FOLDERS_INDEX = "folders-index";
+
+    /**
+     * 
+     */
+    protected FoldersMapping() {
+
+	super(FOLDERS_INDEX);
+    }
+}
