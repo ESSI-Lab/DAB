3740f34cb535e9f9899fbfec43df2a82361e10a5
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-03-11 15:51:51 +0100

    [main-private] udpate in OpenSearchQueryBuilder to use the keyword field for metadata element range queries

--- database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/query/OpenSearchQueryBuilder.java
+++ database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/query/OpenSearchQueryBuilder.java
@@ -61,6 +61,7 @@ import org.opensearch.client.opensearch.core.msearch.MultisearchBody;
 import org.opensearch.client.opensearch.core.msearch.MultisearchHeader;
 import org.opensearch.client.opensearch.core.msearch.RequestItem;
 
+import eu.essi_lab.api.database.SourceStorageWorker.DataFolderIndexDocument;
 import eu.essi_lab.api.database.opensearch.ConversionUtils;
 import eu.essi_lab.api.database.opensearch.OpenSearchFolder;
 import eu.essi_lab.api.database.opensearch.OpenSearchWrapper;
@@ -374,7 +375,11 @@ public class OpenSearchQueryBuilder {
 		value = ConversionUtils.parseToLongString(value);
 	    }
 
-	    return buildRangeQuery(el.getName(), operator, value, ranking.computePropertyWeight(el));
+	    return buildRangeQuery(//
+		    DataFolderMapping.toKeywordField(el.getName()),//
+		    operator,//
+		    value,//
+		    ranking.computePropertyWeight(el));
 
 	case TEXT_SEARCH:
 
