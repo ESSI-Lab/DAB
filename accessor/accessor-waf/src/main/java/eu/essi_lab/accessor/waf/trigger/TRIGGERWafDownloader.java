0dd67d9ae642e655dc3a7470b3230d14d01f21b9
Roberto Roncella <roberto.roncella@cnr.it>
2025-03-20 01:16:38 +0100

    [GIP-764] started TRIGGERWafDownloader implementation

--- accessor/accessor-waf/src/main/java/eu/essi_lab/accessor/waf/trigger/TRIGGERWafDownloader.java
+++ accessor/accessor-waf/src/main/java/eu/essi_lab/accessor/waf/trigger/TRIGGERWafDownloader.java
@@ -23,34 +23,46 @@ package eu.essi_lab.accessor.waf.trigger;
 
 import java.io.File;
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.TimeZone;
 
-import org.geotools.imageio.netcdf.GeoToolsNetCDFReader;
-import org.geotools.imageio.netcdf.utilities.NetCDFCRSUtilities;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import javax.xml.bind.JAXBElement;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
-import eu.essi_lab.access.DataDownloader;
+import org.cuahsi.waterml._1.ObjectFactory;
+import org.cuahsi.waterml._1.TimeSeriesResponseType;
+import org.cuahsi.waterml._1.ValueSingleVariable;
+import org.cuahsi.waterml._1.essi.JAXBWML;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import eu.essi_lab.access.wml.WMLDataDownloader;
+import eu.essi_lab.iso.datamodel.classes.BoundingPolygon;
+import eu.essi_lab.iso.datamodel.classes.GeographicBoundingBox;
+import eu.essi_lab.iso.datamodel.classes.TemporalExtent;
 import eu.essi_lab.lib.net.downloader.Downloader;
+import eu.essi_lab.lib.net.utils.HttpConnectionUtils;
+import eu.essi_lab.lib.utils.GSLoggerFactory;
+import eu.essi_lab.lib.utils.ISO8601DateTimeUtils;
+import eu.essi_lab.model.exceptions.ErrorInfo;
 import eu.essi_lab.model.exceptions.GSException;
 import eu.essi_lab.model.resource.data.CRS;
 import eu.essi_lab.model.resource.data.DataDescriptor;
 import eu.essi_lab.model.resource.data.DataFormat;
 import eu.essi_lab.model.resource.data.DataType;
-import eu.essi_lab.model.resource.data.Datum;
-import eu.essi_lab.model.resource.data.DimensionType;
 import eu.essi_lab.model.resource.data.Unit;
 import eu.essi_lab.model.resource.data.dimension.ContinueDimension;
 import eu.essi_lab.model.resource.data.dimension.DataDimension;
-import eu.essi_lab.netcdf.timeseries.NetCDFUtils;
-import ucar.nc2.Variable;
-import ucar.nc2.constants.AxisType;
-import ucar.nc2.dataset.CoordinateAxis;
-import ucar.nc2.dataset.CoordinateAxis1DTime;
-import ucar.nc2.dataset.NetcdfDataset;
 
-public class TRIGGERWafDownloader extends DataDownloader {
+public class TRIGGERWafDownloader extends WMLDataDownloader {
 
     @Override
     public boolean canDownload() {
@@ -59,144 +71,251 @@ public class TRIGGERWafDownloader extends DataDownloader {
 
     @Override
     public List<DataDescriptor> getRemoteDescriptors() throws GSException {
-	try {
-	    File file = TRIGGERWafConnector.getLocalCopy(online.getLinkage(), online.getName());
-	    List<DataDescriptor> ret = new ArrayList<>();
+	List<DataDescriptor> ret = new ArrayList<>();
 
-	    NetcdfDataset ncDataset = NetcdfDataset.openDataset(file.getAbsolutePath());
+	DataDescriptor descriptor = new DataDescriptor();
+	descriptor.setDataType(DataType.TIME_SERIES);
+	descriptor.setDataFormat(DataFormat.WATERML_1_1());
+	descriptor.setCRS(CRS.EPSG_4326());
 
-	    List<Variable> mainVariables = NetCDFUtils.getGeographicVariables(ncDataset);
-	    Variable mainVariable = mainVariables.get(0);
-	    CoordinateReferenceSystem decodedCRS = GeoToolsNetCDFReader.extractCRS(ncDataset, mainVariable);
-	    CRS crs = CRS.EPSG_4326();
-	    if (decodedCRS != null) {
-		// this workaround is because by default GeoTools NetcdfUtils uses OGC CRS 84
-		// for NetCDF latitude
-		// longitude crs
-		if (decodedCRS.equals(NetCDFCRSUtilities.WGS84)) {
-		    decodedCRS = org.geotools.referencing.CRS.decode("EPSG:4326");
-		}
+	GeographicBoundingBox bbox = resource.getHarmonizedMetadata().getCoreMetadata().getBoundingBox();
+	if (bbox != null) {
+	    Double lat = bbox.getNorth();
+	    Double lon = bbox.getEast();
 
-		crs = CRS.fromGeoToolsCRS(decodedCRS);
-
-	    }
-
-	    List<CoordinateAxis> axes = ncDataset.getCoordinateAxes();
-
-	    ContinueDimension spatialEast = null;
-	    ContinueDimension spatialNorth = null;
-	    ContinueDimension time = null;
-
-	    for (CoordinateAxis axe : axes) {
-		ContinueDimension dimension = new ContinueDimension(NetCDFUtils.getAxisName(axe));
-		double min = axe.getMinValue();
-		double max = axe.getMaxValue();
-		long size = axe.getSize();
-		Double resolution = NetCDFUtils.readResolution(axe.read());
-		Unit uom = Unit.fromIdentifier(NetCDFUtils.getAxisUnit(axe));
-		Datum datum = null;
-
-		AxisType type = axe.getAxisType();
-		if (type == null) {
-		} else {
-		    switch (type) {
-		    case GeoX:
-		    case Lon:
-			spatialEast = dimension;
-			dimension.setType(DimensionType.ROW);
-			break;
-		    case GeoY:
-		    case Lat:
-			spatialNorth = dimension;
-			dimension.setType(DimensionType.COLUMN);
-			break;
-		    case Time:
-			CoordinateAxis1DTime timeAxis = CoordinateAxis1DTime.factory(ncDataset, axe, null);
-			time = dimension;
-			dimension.setType(DimensionType.TIME);
-			Double resT = NetCDFUtils.readMinimumResolution(axe.read());
-			// it means it has a regular time grid (although time units may not be
-			// milliseconds), so we are
-			// authorized to divide
-			if (resT != null && size != 0) {
-			    long milliSeconds = timeAxis.getCalendarDateRange().getDurationInSecs() * 1000;
-			    resolution = (double) milliSeconds / (double) (size - 1);
-			    min = timeAxis.getCalendarDateRange().getStart().getMillis();
-			    max = timeAxis.getCalendarDateRange().getEnd().getMillis();
-			    uom = Unit.MILLI_SECOND;
-			    datum = Datum.UNIX_EPOCH_TIME();
-			}
-			break;
-		    default:
-			break;
-		    }
-		}
-		dimension.setLower(min);
-		dimension.setUpper(max);
-		dimension.setSize(size);
-		dimension.setResolution(resolution);
-		dimension.setDatum(datum);
-		dimension.setUom(uom);
-	    }
-
-	    DataDescriptor descriptor = new DataDescriptor();
-
-	    descriptor.setDataType(DataType.GRID);
-
-	    descriptor.setCRS(crs);
-
-	    descriptor.setDataFormat(DataFormat.NETCDF());
-
-	    List<DataDimension> spatialDimensions = crs.getDefaultDimensions();
-
-	    DataDimension dimension1 = spatialDimensions.get(0);
-
-	    dimension1.getContinueDimension().setResolution(spatialNorth.getResolution().doubleValue());
-	    dimension1.getContinueDimension().setSize(spatialNorth.getSize());
-	    dimension1.getContinueDimension().setLower(spatialNorth.getLower());
-	    dimension1.getContinueDimension().setUpper(spatialNorth.getUpper());
-
-	    DataDimension dimension2 = spatialDimensions.get(1);
-
-	    dimension2.getContinueDimension().setResolution(spatialEast.getResolution().doubleValue());
-	    dimension2.getContinueDimension().setSize(spatialEast.getSize());
-	    dimension2.getContinueDimension().setLower(spatialEast.getLower());
-	    dimension2.getContinueDimension().setUpper(spatialEast.getUpper());
-
-	    descriptor.setSpatialDimensions(spatialDimensions);
-
-	    descriptor.setTemporalDimension(time);
-
-	    ret.add(descriptor);
-	    return ret;
-	} catch (Exception e) {
-	    e.printStackTrace();
+	    descriptor.setEPSG4326SpatialDimensions(lat, lon);
+	    descriptor.getFirstSpatialDimension().getContinueDimension().setSize(1l);
+	    descriptor.getSecondSpatialDimension().getContinueDimension().setSize(1l);
+	    descriptor.getFirstSpatialDimension().getContinueDimension().setLowerTolerance(0.01);
+	    descriptor.getFirstSpatialDimension().getContinueDimension().setUpperTolerance(0.01);
+	    descriptor.getSecondSpatialDimension().getContinueDimension().setLowerTolerance(0.01);
+	    descriptor.getSecondSpatialDimension().getContinueDimension().setUpperTolerance(0.01);
 	}
-	return null;
+
+	Iterator<BoundingPolygon> iterator = resource.getHarmonizedMetadata().getCoreMetadata().getDataIdentification()
+		.getBoundingPolygons();
+	if (iterator != null) {
+	    while (iterator.hasNext()) {
+		BoundingPolygon polygon = iterator.next();
+		List<List<Double>> multiPoints = polygon.getMultiPoints();
+		for (List<Double> m : multiPoints) {
+		    Double lat = m.get(0);
+		    Double lon = m.get(1);
+		    descriptor.setEPSG4326SpatialDimensions(lat, lon);
+		    descriptor.getFirstSpatialDimension().getContinueDimension().setSize(1l);
+		    descriptor.getSecondSpatialDimension().getContinueDimension().setSize(1l);
+		    descriptor.getFirstSpatialDimension().getContinueDimension().setLowerTolerance(0.01);
+		    descriptor.getFirstSpatialDimension().getContinueDimension().setUpperTolerance(0.01);
+		    descriptor.getSecondSpatialDimension().getContinueDimension().setLowerTolerance(0.01);
+		    descriptor.getSecondSpatialDimension().getContinueDimension().setUpperTolerance(0.01);
+		}
+	    }
+	}
+
+	//
+	// temp extent
+	//
+	TemporalExtent extent = resource.getHarmonizedMetadata().getCoreMetadata().getTemporalExtent();
+
+	String startDate = extent.getBeginPosition();
+	String endDate = extent.getEndPosition();
+
+	if (extent.isEndPositionIndeterminate()) {
+	    endDate = ISO8601DateTimeUtils.getISO8601DateTime();
+	}
+
+	Optional<Date> optionalBegin = ISO8601DateTimeUtils.parseISO8601ToDate(startDate);
+	Optional<Date> optionalEnd = ISO8601DateTimeUtils.parseISO8601ToDate(endDate);
+
+	if (optionalBegin.isPresent() && optionalEnd.isPresent()) {
+
+	    Date begin = optionalBegin.get();
+	    Date end = optionalEnd.get();
+
+	    descriptor.setTemporalDimension(begin, end);
+
+	    DataDimension temporalDimension = descriptor.getTemporalDimension();
+	    Long oneDayInMilliseconds = 1000 * 60 * 60 * 24l;
+
+	    temporalDimension.getContinueDimension().setLowerTolerance(oneDayInMilliseconds);
+	    temporalDimension.getContinueDimension().setUpperTolerance(oneDayInMilliseconds);
+	}
+
+	ret.add(descriptor);
+
+	return ret;
     }
 
     @Override
     public File download(DataDescriptor descriptor) throws GSException {
-	File file;
+	Exception ex = null;
+
 	try {
-	    file = TRIGGERWafConnector.getLocalCopy(online.getLinkage(), online.getName());
-	    return file;
-	} catch (Exception e) {
-	    e.printStackTrace();
+
+	    Date begin = null;
+	    Date end = null;
+
+	    ObjectFactory factory = new ObjectFactory();
+
+	    String startString = null;
+	    String endString = null;
+
+	    DataDimension dimension = descriptor.getTemporalDimension();
+
+	    if (dimension != null && dimension.getContinueDimension().getUom().equals(Unit.MILLI_SECOND)) {
+
+		ContinueDimension sizedDimension = dimension.getContinueDimension();
+
+		begin = new Date(sizedDimension.getLower().longValue());
+		end = new Date(sizedDimension.getUpper().longValue());
+
+		startString = ISO8601DateTimeUtils.getISO8601DateTime(begin);
+		endString = ISO8601DateTimeUtils.getISO8601DateTime(end);
+	    }
+
+	    if (startString == null || endString == null) {
+
+		startString = ISO8601DateTimeUtils.getISO8601Date(new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000L));
+		endString = ISO8601DateTimeUtils.getISO8601Date(new Date());
+	    }
+
+	    JSONObject ret = new JSONObject();
+	    String linkage = online.getLinkage();
+	    String name = online.getName();
+	    String[] splittedId = name.split(":");
+
+	    String variableName = splittedId[1];
+	    String stationId = splittedId[0];
+	    String units = splittedId[2];
+
+	    Optional<Date> startDate = ISO8601DateTimeUtils.parseISO8601ToDate(startString);
+	    Optional<Date> endDate = ISO8601DateTimeUtils.parseISO8601ToDate(endString);
+
+	    List<Date[]> dates = new ArrayList<Date[]>();
+	    if (startDate.isPresent() && endDate.isPresent()) {
+		dates = checkDates(startDate.get(), endDate.get());
+	    }
+
+	    TimeSeriesResponseType tsrt = getTimeSeriesTemplate();
+	    DatatypeFactory xmlFactory = DatatypeFactory.newInstance();
+
+	    File tempFile;
+	    boolean isTemperature = false;
+
+//	    if (units.contains("°C") || units.contains("ºC") || units.contains("K")) {
+//		units = units.replace("°C", "K").replace("ºC", "K");
+//		isTemperature = true;
+//	    }
+
+	    for (Date[] d : dates) {
+
+		startString = convertISODateToAcronetDate(ISO8601DateTimeUtils.getISO8601DateTime(d[0]));
+		endString = convertISODateToAcronetDate(ISO8601DateTimeUtils.getISO8601DateTime(d[1]));
+		ret = client.getData(variableName, stationId, startString, endString);
+		int count = 0;
+		if (ret != null && !ret.isEmpty()) {
+
+		    JSONArray data = ret.optJSONArray("values");
+		    JSONArray timeLine = ret.optJSONArray("timeline");
+		    System.out.println(data.length());
+		    System.out.println(timeLine.length());
+		    for (int j = 0; j < data.length(); j++) {
+
+			JSONArray dataTime = data.optJSONArray(j);
+
+			BigDecimal value = data.optBigDecimal(j, null);// data.optBigDecimal(1, null);//
+								       // obj.optBigDecimal(varId.toLowerCase(),
+								       // null);// data.optString("value");
+			ValueSingleVariable variable = new ValueSingleVariable();
+
+			if (value != null && value.doubleValue() != -9998.0) {
+
+			    //
+			    // value
+			    //
+
+			    // BigDecimal dataValue = new BigDecimal(value);
+			    if (isTemperature) {
+				// from Celsius to Kelvin
+				BigDecimal kelvin = new BigDecimal("273.15");
+				value = value.add(kelvin);
+			    }
+
+			    value = value.setScale(2, BigDecimal.ROUND_FLOOR);
+			    // int valueInteger = value.multiply(new BigDecimal(100)).intValue();
+
+			    variable.setValue(value);
+
+			    String date = timeLine.optString(j, null);
+
+			    if (date != null) {
+
+				Optional<Date> parsed = ISO8601DateTimeUtils.parseNotStandard2ToDate(date);
+
+				// Date parsed =
+				// Date.from(dateTime.atZone(ZoneId.of(TimeZone.getTimeZone("GMT").getID())).toInstant());
+
+				if (parsed != null && parsed.isPresent()) {
+
+				    // Date parsed = iso8601OutputFormat.parse(date);
+
+				    GregorianCalendar gregCal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+				    gregCal.setTime(parsed.get());
+
+				    XMLGregorianCalendar xmlGregCal = xmlFactory.newXMLGregorianCalendar(gregCal);
+				    variable.setDateTimeUTC(xmlGregCal);
+
+				    //
+				    //
+				    //
+
+				    addValue(tsrt, variable);
+				    count++;
+				}
+			    }
+			}
+		    }
+
+		}
+		if(count > 0)
+		    break;
+	    }
+
+	    JAXBElement<TimeSeriesResponseType> response = factory.createTimeSeriesResponse(tsrt);
+	    tempFile = File.createTempFile(getClass().getSimpleName(), ".wml");
+
+	    tempFile.deleteOnExit();
+	    JAXBWML.getInstance().marshal(response, tempFile);
+
+	    return tempFile;
+
+	} catch (
+
+	Exception e) {
+
+	    ex = e;
 	}
-	
-	return null;
+
+	throw GSException.createException(//
+
+		getClass(), //
+		ex.getMessage(), //
+		null, //
+		ErrorInfo.ERRORTYPE_INTERNAL, //
+		ErrorInfo.SEVERITY_ERROR, //
+		"TRIGGER WAF DOWNLOAD ERROR");
     }
 
     @Override
     public boolean canConnect() throws GSException {
-	Downloader downloader = new Downloader();
-	Optional<InputStream> stream = downloader.downloadOptionalStream(online.getLinkage());
-	if (stream.isPresent()) {
-	    return true;
-	} else {
-	    return false;
-	}
+//	try {
+//	    return HttpConnectionUtils.checkConnectivity(online.getLinkage());
+//	} catch (URISyntaxException e) {
+//
+//	    GSLoggerFactory.getLogger(getClass()).error(e);
+//	}
+//
+//	return false;
     }
 
     @Override
