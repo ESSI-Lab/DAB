f22760969e4f6f8a2872387ff9c0633fcb95ce3d
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-03-29 16:36:18 +0100

    [main-private] update in OpenSearchFinder to init the map updater task in 5 minutes

--- database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/OpenSearchFinder.java
+++ database-api-elasticsearch-impl/src/main/java/eu/essi_lab/api/database/opensearch/OpenSearchFinder.java
@@ -152,7 +152,7 @@ public class OpenSearchFinder implements DatabaseFinder {
 	    MAP_UPDATER_TASK = new CachedMapUpdater();
 
 	    Timer timer = new Timer();
-	    timer.scheduleAtFixedRate(MAP_UPDATER_TASK, TimeUnit.MINUTES.toMillis(15), MAP_UPDATE_PERIOD);
+	    timer.scheduleAtFixedRate(MAP_UPDATER_TASK, TimeUnit.MINUTES.toMillis(5), MAP_UPDATE_PERIOD);
 	}
 
 	MAP_UPDATER_TASK.setOpenSearchFinder(this);
@@ -435,6 +435,7 @@ public class OpenSearchFinder implements DatabaseFinder {
 
     /**
      * @param sourceIds
+     * @param useCache
      * @return
      * @throws GSException
      */
