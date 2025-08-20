955cd7bc7aef372a71cfb665c7fa814b12a7d4c5
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-08-20 08:07:11 +0200

    [main-private] minor change

--- jaxb-classes/jaxb-classes-wms-1.3.0/src/test/java/eu/essi_lab/jaxb/wms/extension/JAXBWMSTest.java
+++ jaxb-classes/jaxb-classes-wms-1.3.0/src/test/java/eu/essi_lab/jaxb/wms/extension/JAXBWMSTest.java
@@ -1,13 +1,24 @@
 package eu.essi_lab.jaxb.wms.extension;
 
-import javax.xml.bind.JAXBException;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
+import java.util.ServiceLoader;
+
+import javax.xml.bind.JAXBContextFactory;
+import javax.xml.bind.JAXBException;
 
 public class JAXBWMSTest {
 
-    @Test
+    @org.junit.Test
     public void test() throws JAXBException {
+
+	ServiceLoader<JAXBContextFactory> serviceLoader = ServiceLoader.load(JAXBContextFactory.class);
+
+	// com.sun.xml.bind.v2.JAXBContextFactory
+	JAXBContextFactory jaxbContextFactory = serviceLoader.findFirst().get();
+
+	assertNotNull(jaxbContextFactory);
+
 	JAXBWMS.getInstance().getMarshaller();
     }
 
