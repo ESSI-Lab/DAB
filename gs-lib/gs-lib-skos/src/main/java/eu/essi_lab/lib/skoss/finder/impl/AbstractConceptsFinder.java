9127bbc286a7ce840c9cc7cd444866f9089f7a8c
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-10-08 17:20:29 +0200

    [GIP-785] minor refactoring

--- gs-lib/gs-lib-skos/src/main/java/eu/essi_lab/lib/skoss/finder/impl/AbstractConceptsFinder.java
+++ gs-lib/gs-lib-skos/src/main/java/eu/essi_lab/lib/skoss/finder/impl/AbstractConceptsFinder.java
@@ -4,14 +4,16 @@
 package eu.essi_lab.lib.skoss.finder.impl;
 
 import eu.essi_lab.lib.skoss.finder.ConceptsFinder;
+import eu.essi_lab.lib.skoss.finder.ConceptsQueryExecutor;
 import eu.essi_lab.lib.skoss.finder.FindConceptsQueryBuilder;
 
 /**
  * @author Fabrizio
  */
-public abstract class AbstractConceptsFinder implements ConceptsFinder{
+public abstract class AbstractConceptsFinder implements ConceptsFinder {
 
     private FindConceptsQueryBuilder builder;
+    private ConceptsQueryExecutor executor;
 
     /**
      * 
@@ -35,5 +37,21 @@ public abstract class AbstractConceptsFinder implements ConceptsFinder{
     public void setQueryBuilder(FindConceptsQueryBuilder builder) {
 
 	this.builder = builder;
+    }
+
+    /**
+     * @return the executor
+     */
+    public ConceptsQueryExecutor getExecutor() {
+
+	return executor;
+    }
+
+    /**
+     * @param executor
+     */
+    public void setExecutor(ConceptsQueryExecutor executor) {
+
+	this.executor = executor;
     }
 }
--- gs-lib/gs-lib-skos/src/main/java/eu/essi_lab/lib/skoss/finder/impl/AbstractFedXConceptsFinder.java
+++ gs-lib/gs-lib-skos/src/main/java/eu/essi_lab/lib/skoss/finder/impl/AbstractFedXConceptsFinder.java
@@ -3,14 +3,10 @@
  */
 package eu.essi_lab.lib.skoss.finder.impl;
 
-import eu.essi_lab.lib.skoss.finder.ConceptsQueryExecutor;
-
 /**
  * @author Fabrizio
  */
 public abstract class AbstractFedXConceptsFinder extends AbstractConceptsFinder {
-
-    private ConceptsQueryExecutor executor;
 
     /**
      * 
@@ -18,21 +14,5 @@ public abstract class AbstractFedXConceptsFinder extends AbstractConceptsFinder 
     public AbstractFedXConceptsFinder() {
 
 	setExecutor(new FedXConceptsQueryExecutor());
-    }
-
-    /**
-     * @return the executor
-     */
-    public ConceptsQueryExecutor getExecutor() {
-
-	return executor;
-    }
-
-    /**
-     * @param executor
-     */
-    public void setExecutor(ConceptsQueryExecutor executor) {
-
-	this.executor = executor;
     }
 }
