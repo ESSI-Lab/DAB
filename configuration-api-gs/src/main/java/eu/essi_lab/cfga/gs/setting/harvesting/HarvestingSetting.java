5147b5ea4c16e8c2e963addca97406e4f1564ba9
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-11-14 15:45:40 +0100

    [main-private] minor changes

--- configuration-api-gs/src/main/java/eu/essi_lab/cfga/gs/setting/harvesting/HarvestingSetting.java
+++ configuration-api-gs/src/main/java/eu/essi_lab/cfga/gs/setting/harvesting/HarvestingSetting.java
@@ -187,7 +187,7 @@ public abstract class HarvestingSetting extends SchedulerWorkerSetting implement
 
 	    TabDescriptor tabDescriptor = TabDescriptorBuilder.get().//
 		    withIndex(GSTabIndex.HARVESTING.getIndex()).//
-		    withShowDirective("Harvesting", "With this tab you can handle the DAB brokered sources. Click \"Reload\" to"
+		    withShowDirective("Harvesting", "With this tab you can handle the DAB harvested sources. Click \"Reload\" to"
 		    + "update the scheduler information",SortDirection.ASCENDING).//
 
 		    withAddDirective(//
