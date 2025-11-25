fb6f0de701ebc167ebbd9a0ff32e83fc81cc8c5c
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-11-25 10:52:15 +0100

    [GIP-792] GSTabIndex updated for the brokering tab

--- configuration-api-gs/src/main/java/eu/essi_lab/cfga/gs/GSTabIndex.java
+++ configuration-api-gs/src/main/java/eu/essi_lab/cfga/gs/GSTabIndex.java
@@ -23,12 +23,12 @@ import eu.essi_lab.cfga.gui.TabIndex;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -41,7 +41,12 @@ public enum GSTabIndex implements TabIndex {
     /**
      *
      */
-    BROKERING(0),
+    BROKERING(0, false,//
+	    Descriptor.of(s -> s.getSettingClass().getSimpleName().equals("HarvestingSettingImpl")),//
+	    Descriptor.of(s -> s.getSettingClass().equals(DistributionSetting.class)),//
+	    Descriptor.of(s -> s.getSettingClass().getSuperclass().equals(ProfilerSetting.class)),//
+	    Descriptor.of(s -> s.getSettingClass().getSuperclass().getSuperclass().equals(ProfilerSetting.class))//
+    ),
     /**
      *
      */
