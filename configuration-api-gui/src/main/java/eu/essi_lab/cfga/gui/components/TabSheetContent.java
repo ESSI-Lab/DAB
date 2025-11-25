67b66f0ada4971c038e4ef53316bb1c934668613
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-11-25 13:54:38 +0100

    [GIP-792] fix in TabSheetContent to correctly handle the tabs content

--- configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/TabSheetContent.java
+++ configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/TabSheetContent.java
@@ -10,12 +10,12 @@ package eu.essi_lab.cfga.gui.components;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -25,12 +25,15 @@ import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.tabs.*;
 import eu.essi_lab.cfga.gui.extension.*;
 
+import java.util.*;
+
 /**
  * @author Fabrizio
  */
 public class TabSheetContent extends TabSheet implements Renderable {
 
     private boolean rendered;
+    private final List<TabContent> list;
 
     /**
      *
@@ -38,16 +41,18 @@ public class TabSheetContent extends TabSheet implements Renderable {
     public TabSheetContent() {
 
 	setWidthFull();
+	list = new ArrayList<>();
     }
 
     /**
-     *
      * @param desc
-     * @param container
+     * @param content
      */
-    public void add(TabDescriptor desc, TabContent container){
+    public void add(TabDescriptor desc, TabContent content) {
 
-	add(desc.getLabel(), container);
+	add(desc.getLabel(), content);
+
+	list.add(content);
     }
 
     @Override
@@ -59,7 +64,7 @@ public class TabSheetContent extends TabSheet implements Renderable {
     @Override
     public void render(boolean refresh) {
 
-	this.getChildren().map(comp -> (Renderable)comp).forEach(rend -> rend.render(refresh));
+	list.forEach(rend -> rend.render(refresh));//
     }
 
     @Override
