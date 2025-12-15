03077d539f6750ae21826336529b3606e7819656
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2025-12-15 16:37:15 +0100

    [DEP-VULN] udpate to avoid null pointer

--- security/src/main/java/eu/essi_lab/authorization/xacml/XACMLAuthorizer.java
+++ security/src/main/java/eu/essi_lab/authorization/xacml/XACMLAuthorizer.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package eu.essi_lab.authorization.xacml;
 
@@ -13,12 +13,12 @@ package eu.essi_lab.authorization.xacml;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -122,18 +122,23 @@ public class XACMLAuthorizer implements Closeable, MessageAuthorizer<RequestMess
 	logBuilder.append("\n- User role: " + role);
 
 	Optional<View> optionalView = message.getView();
+
 	if (optionalView.isPresent()) {
+
 	    View view = optionalView.get();
 	    String creator = view.getCreator();
-	    switch (creator) {
-	    case "whos":
-	    case "his-central":
-		if (role.equals("anonymous")) {
-		    return false;
+
+	    if (creator != null) {
+		switch (creator) {
+		case "whos":
+		case "his-central":
+		    if (role.equals("anonymous")) {
+			return false;
+		    }
+		    break;
+		default:
+		    break;
 		}
-		break;
-	    default:
-		break;
 	    }
 	}
 
