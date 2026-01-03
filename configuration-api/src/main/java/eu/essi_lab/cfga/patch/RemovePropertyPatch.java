3b9da425eb41642b03ed4ff4d7dd7df0f98a315e
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-01-03 12:20:07 +0100

    [main-private] RemovePropertyPath updated to include optional value

--- configuration-api/src/main/java/eu/essi_lab/cfga/patch/RemovePropertyPatch.java
+++ configuration-api/src/main/java/eu/essi_lab/cfga/patch/RemovePropertyPatch.java
@@ -10,12 +10,12 @@ package eu.essi_lab.cfga.patch;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -33,6 +33,7 @@ import java.util.*;
 public class RemovePropertyPatch extends Patch {
 
     private final Property<?> property;
+    private Object value;
 
     /**
      * @param configuration
@@ -41,18 +42,31 @@ public class RemovePropertyPatch extends Patch {
      */
     public static RemovePropertyPatch of(Configuration configuration, Property<?> property) {
 
-	return new RemovePropertyPatch(configuration, property);
+	return new RemovePropertyPatch(configuration, property, null);
     }
 
     /**
      * @param configuration
      * @param property
+     * @param value
+     * @return
      */
-    private RemovePropertyPatch(Configuration configuration, Property<?> property) {
+    public static RemovePropertyPatch of(Configuration configuration, Property<?> property, Object value) {
+
+	return new RemovePropertyPatch(configuration, property, value);
+    }
+
+    /**
+     * @param configuration
+     * @param property
+     * @param value
+     */
+    private RemovePropertyPatch(Configuration configuration, Property<?> property, Object value) {
 
 	setConfiguration(configuration);
 
 	this.property = property;
+	this.value = value;
     }
 
     /**
@@ -63,32 +77,27 @@ public class RemovePropertyPatch extends Patch {
 
 	ArrayList<Setting> list = new ArrayList<>();
 
-	getConfiguration().list().forEach(s -> {
+	getConfiguration().list().forEach(setting -> {
 
-	    String object = s.getObject().toString();
+	    if (!find(setting,property,value).isEmpty()) {
 
-	    if (object.contains(property.getKey())) {
-
-		list.add(s);
+		list.add(setting);
 	    }
 	});
 
-	GSLoggerFactory.getLogger(getClass())
-		.debug("Found {} settings with legacy {} properties to patch", list.size(), property.getName());
+	GSLoggerFactory.getLogger(getClass()).debug("Found {} settings with {} property to patch", list.size(), property.getName());
 
 	if (!list.isEmpty()) {
 
 	    List<Setting> converted = list.//
 		    stream().//
-		    map(s -> {
+		    map(setting -> {
 
-		ArrayList<Setting> l = new ArrayList<>();
-
-		SettingUtils.deepFind(s, inner -> s.getObject().has(property.getKey()), l);
+		List<Setting> l = find(setting,property,value);
 
 		l.forEach(si -> si.getObject().remove(property.getKey()));
 
-		return s.getObject();
+		return setting.getObject();
 	    }).//
 		    map(Setting::new).//
 		    toList();
@@ -105,5 +114,22 @@ public class RemovePropertyPatch extends Patch {
 	}
 
 	return false;
+    }
+
+    /**
+     *
+     * @param setting
+     * @param property
+     * @param value
+     * @return
+     */
+    private List<Setting> find(Setting setting, Property<?> property, Object value) {
+
+	ArrayList<Setting> list = new ArrayList<>();
+
+	SettingUtils.deepFind(setting, child -> child.getObject().has(property.getKey()) && //
+		(value == null || child.getObject().get(property.getKey()).equals(value)), list);
+
+	return list;
     }
 }
