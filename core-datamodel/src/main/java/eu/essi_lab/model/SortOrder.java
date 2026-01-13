0543a95985ede79bd3b1f26f50b0834a2a651fec
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-01-13 14:08:44 +0100

    [main-private] added SortOrder.of method

--- core-datamodel/src/main/java/eu/essi_lab/model/SortOrder.java
+++ core-datamodel/src/main/java/eu/essi_lab/model/SortOrder.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package eu.essi_lab.model;
 
@@ -13,12 +13,12 @@ package eu.essi_lab.model;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -26,17 +26,19 @@ package eu.essi_lab.model;
 
 import eu.essi_lab.lib.utils.LabeledEnum;
 
+import java.util.*;
+
 /**
  * @author Fabrizio
  */
 public enum SortOrder implements LabeledEnum {
 
     /**
-     * 
+     *
      */
     ASCENDING("Ascending"),
     /**
-     * 
+     *
      */
     DESCENDING("Descending");
 
@@ -51,7 +53,25 @@ public enum SortOrder implements LabeledEnum {
     }
 
     /**
-     * 
+     *
+     * @param label
+     * @return
+     */
+    public static Optional<SortOrder> of(String label) {
+
+	if (label.equals(ASCENDING.label) || label.equals("asc")) {
+	    return Optional.of(SortOrder.ASCENDING);
+	}
+
+	if (label.equals(DESCENDING.label) || label.equals("desc")) {
+	    return Optional.of(SortOrder.DESCENDING);
+	}
+
+	return Optional.empty();
+    }
+
+    /**
+     *
      */
     public String getLabel() {
 
