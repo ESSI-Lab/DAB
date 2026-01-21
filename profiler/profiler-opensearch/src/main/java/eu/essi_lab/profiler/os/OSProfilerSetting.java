0f21127331df4cc0cabe339fa811b19c0f1d1aac
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-01-21 11:02:29 +0100

    [main-private] OSRequestTransformer updated to read optional value of max window size from the setting (default = unlimited)

--- profiler/profiler-opensearch/src/main/java/eu/essi_lab/profiler/os/OSProfilerSetting.java
+++ profiler/profiler-opensearch/src/main/java/eu/essi_lab/profiler/os/OSProfilerSetting.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package eu.essi_lab.profiler.os;
 
@@ -13,12 +13,12 @@ package eu.essi_lab.profiler.os;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -32,12 +32,17 @@ import eu.essi_lab.cfga.gs.setting.ProfilerSetting;
 public class OSProfilerSetting extends ProfilerSetting {
 
     /**
-     * 
+     *
      */
     public static final String OPEN_SEARCH_PROFILER_TYPE = "OpenSearch";
 
     /**
-     * 
+     *
+     */
+    public static final String MAX_RESULT_WINDOW_SIZE = "maxResultWindowSize";
+
+    /**
+     *
      */
     public OSProfilerSetting() {
 
