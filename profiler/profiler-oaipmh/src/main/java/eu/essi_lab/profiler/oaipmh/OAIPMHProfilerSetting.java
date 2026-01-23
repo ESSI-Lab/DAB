7a8c15ee434321d657c54a5e148dc9fe09442d3b
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-01-23 16:03:03 +0100

    [main-private] minor improvement in OAIPMH profiler

--- profiler/profiler-oaipmh/src/main/java/eu/essi_lab/profiler/oaipmh/OAIPMHProfilerSetting.java
+++ profiler/profiler-oaipmh/src/main/java/eu/essi_lab/profiler/oaipmh/OAIPMHProfilerSetting.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package eu.essi_lab.profiler.oaipmh;
 
@@ -13,18 +13,19 @@ package eu.essi_lab.profiler.oaipmh;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
 
-import eu.essi_lab.cfga.gs.setting.ProfilerSetting;
+import eu.essi_lab.cfga.gs.setting.*;
+import eu.essi_lab.lib.utils.*;
 
 /**
  * @author Fabrizio
@@ -32,7 +33,40 @@ import eu.essi_lab.cfga.gs.setting.ProfilerSetting;
 public class OAIPMHProfilerSetting extends ProfilerSetting {
 
     /**
-     * 
+     * @author Fabrizio
+     */
+    public enum KeyValueOptionKeys implements LabeledEnum {
+
+	/**
+	 *
+	 */
+	PAGE_SIZE("pageSize");
+
+	private String name;
+
+	/**
+	 * @param name
+	 */
+	private KeyValueOptionKeys(String name) {
+
+	    this.name = name;
+	}
+
+	@Override
+	public String toString() {
+
+	    return getLabel();
+	}
+
+	@Override
+	public String getLabel() {
+
+	    return name;
+	}
+    }
+
+    /**
+     *
      */
     public OAIPMHProfilerSetting() {
 
