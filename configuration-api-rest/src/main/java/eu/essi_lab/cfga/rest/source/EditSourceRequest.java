2f0e510be2f0abb44b855ac112a3ff0789e59d7f
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-02-11 09:49:24 +0100

    [GIP-798] created configuration-api-rest project

---
+++ configuration-api-rest/src/main/java/eu/essi_lab/cfga/rest/source/EditSourceRequest.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package eu.essi_lab.cfga.rest.source;
+
+import eu.essi_lab.cfga.option.*;
+import eu.essi_lab.cfga.rest.*;
+import eu.essi_lab.model.Queryable.*;
+import org.json.*;
+
+import java.util.*;
+
+/**
+ * @author Fabrizio
+ */
+public class EditSourceRequest extends PutSourceRequest {
+
+    /**
+     * 
+     */
+    public EditSourceRequest() {
+    }
+
+    /**
+     * @param object
+     */
+    public EditSourceRequest(JSONObject object) {
+
+	super(object);
+    }
+
+    @Override
+    public List<Parameter> getSupportedParameters() {
+
+	ArrayList<Parameter> list = new ArrayList<>();
+
+	list.add(Parameter.of(SOURCE_ID, ContentType.TEXTUAL, InputPattern.ALPHANUMERIC_AND_UNDERSCORE_AND_MINUS, true));
+	list.add(Parameter.of(SOURCE_LABEL, ContentType.TEXTUAL, false));
+	list.add(Parameter.of(SOURCE_ENDPOINT, ContentType.TEXTUAL, false));
+	list.add(Parameter.of(SERVICE_TYPE, ContentType.TEXTUAL, SourceType.class, false));
+
+	return list;
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected void mandatoryCheck() {
+
+	super.mandatoryCheck();
+
+	if (readParameters().size() == 1) {
+
+	    throw new IllegalArgumentException("At least one of the parameters '" + SOURCE_LABEL + "', '" + SOURCE_ENDPOINT + "', '"
+		    + SERVICE_TYPE + "' must be provided'");
+	}
+    }
+}
