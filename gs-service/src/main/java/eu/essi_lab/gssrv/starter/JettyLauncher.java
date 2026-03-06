ae886d5a8db6d03a4a1fabbc852c52138e7ee288
Enrico Boldrini <enrico.boldrini@cnr.it>
2026-03-06 11:40:57 +0100

    [main] jetty launcher

--- gs-service/src/main/java/eu/essi_lab/gssrv/starter/JettyLauncher.java
+++ gs-service/src/main/java/eu/essi_lab/gssrv/starter/JettyLauncher.java
@@ -1,11 +1,32 @@
 package eu.essi_lab.gssrv.starter;
 
+/*-
+ * #%L
+ * Discovery and Access Broker (DAB)
+ * %%
+ * Copyright (C) 2021 - 2026 National Research Council of Italy (CNR)/Institute of Atmospheric Pollution Research (IIA)/ESSI-Lab
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import eu.essi_lab.lib.utils.GSLoggerFactory;
 import eu.essi_lab.messages.*;
 import org.eclipse.jetty.ee11.annotations.*;
 import org.eclipse.jetty.ee11.webapp.*;
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.util.resource.*;
-
 
 import java.nio.file.*;
 
@@ -16,7 +37,7 @@ import java.nio.file.*;
  */
 public class JettyLauncher {
 
-    private static final String	CONTEXT_PATH = "/gs-service";
+    private static final String CONTEXT_PATH = "/gs-service";
     private static final int MAX_REQUEST_HEADER_SIZE = 16384;
 
     /**
@@ -32,21 +53,17 @@ public class JettyLauncher {
 	HttpConfiguration httpConfig = new HttpConfiguration();
 	httpConfig.setRequestHeaderSize(MAX_REQUEST_HEADER_SIZE);
 
-	ServerConnector connector =
-		new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+	ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
 	connector.setPort(JVMOption.getIntValue(JVMOption.JETTY_LAUNCHER_PORT).get());
 	server.addConnector(connector);
 
 	WebAppContext webapp = new WebAppContext();
 
-	webapp.setExtraClasspath(
-		Path.of("target/classes")
-			.toAbsolutePath()
-			.toString()
-	);
-
 	webapp.setContextPath(CONTEXT_PATH);
-	webapp.setWar("src/main/webapp");
+	Path devWebapp = Path.of("src/main/webapp");
+
+	webapp.setWar(devWebapp.toString());
+	webapp.setExtraClasspath(Path.of("target/classes").toAbsolutePath().toString());
 
 	webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
 		".*/spring-[^/]*\\.jar$|.*vaadin-[^/]*\\.jar$|.*flow-[^/]*\\.jar$");
