1a36146777b8b39086e87cb74d79dd8b7c00a369
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-03-15 17:24:37 +0100

    [main-private] improved SettingComponent tab view with options tab

--- configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/setting/SettingComponent.java
+++ configuration-api-gui/src/main/java/eu/essi_lab/cfga/gui/components/setting/SettingComponent.java
@@ -10,12 +10,12 @@ package eu.essi_lab.cfga.gui.components.setting;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -459,7 +459,11 @@ public class SettingComponent extends Div {
 			((VerticalLayout) tabSheet.getComponent(tabSheet.getTabAt(i))).add(mainLayout);
 		    }
 		}
+	    } else if(!setting.getOptions().isEmpty()){
+
+		tabSheet.add("Options", mainLayout);
 	    }
+
 	} else {
 
 	    add(mainLayout);
