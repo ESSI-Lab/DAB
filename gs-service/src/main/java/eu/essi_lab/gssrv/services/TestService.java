c7f2418b9bfe8cdec2c9c6982e14d34b892acb56
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-03-18 14:44:59 +0100

    [GIP-800] created test implementation of ManagedService

---
+++ gs-service/src/main/java/eu/essi_lab/gssrv/services/TestService.java
@@ -0,0 +1,62 @@
+package eu.essi_lab.gssrv.services;
+
+import eu.essi_lab.lib.net.services.*;
+import eu.essi_lab.lib.utils.*;
+
+import java.time.*;
+import java.time.temporal.*;
+
+/**
+ * @author Fabrizio
+ */
+public class TestService implements ManagedService {
+
+    private volatile boolean running = false;
+    private String id;
+    private boolean enabled;
+
+    /**
+     *
+     */
+    public TestService() {
+    }
+
+    /**
+     * @param id
+     */
+    @Override
+    public void setId(String id) {
+
+	this.id = id;
+    }
+
+    @Override
+    public String getId() {
+
+	return id;
+    }
+
+    @Override
+    public void start() {
+
+	running = true;
+
+	new Thread(() -> {
+	    while (running) {
+
+		GSLoggerFactory.getLogger(getClass()).info("*** [ Running service: {} ] ***", getId());
+		try {
+		    Thread.sleep(Duration.of(5, ChronoUnit.SECONDS));
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}).start();
+    }
+
+    @Override
+    public void stop() {
+
+	running = false;
+    }
+}
