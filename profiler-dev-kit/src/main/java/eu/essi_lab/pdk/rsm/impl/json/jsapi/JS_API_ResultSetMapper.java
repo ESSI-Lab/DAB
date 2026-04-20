c3932ad4437bfad66f49149369fad8cc4bec9ef6
Fabrizio Papeschi <fabrizio.papeschi@gmail.com>
2026-04-20 15:07:48 +0200

    [SPAT-REP-MAPPER] TBC

--- profiler-dev-kit/src/main/java/eu/essi_lab/pdk/rsm/impl/json/jsapi/JS_API_ResultSetMapper.java
+++ profiler-dev-kit/src/main/java/eu/essi_lab/pdk/rsm/impl/json/jsapi/JS_API_ResultSetMapper.java
@@ -10,12 +10,12 @@ package eu.essi_lab.pdk.rsm.impl.json.jsapi;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -34,10 +34,10 @@ import eu.essi_lab.model.pluggable.*;
 import eu.essi_lab.model.resource.*;
 import eu.essi_lab.model.resource.data.*;
 import eu.essi_lab.pdk.rsm.*;
+import jakarta.ws.rs.core.*;
 import net.opengis.iso19139.gmx.v_20060504.*;
 import org.json.*;
 
-import jakarta.ws.rs.core.*;
 import javax.xml.datatype.*;
 import java.net.*;
 import java.util.*;
@@ -399,6 +399,11 @@ public class JS_API_ResultSetMapper extends DiscoveryResultSetMapper<String> {
 	// -----------
 
 	normalizeText(firstId.getAbstract()).ifPresent(desc -> report.put("description", desc));
+
+	JSONSpatialRepresentationMapper mapper = new JSONSpatialRepresentationMapper(
+		resource.getHarmonizedMetadata().getCoreMetadata().getMIMetadata());
+
+	JSONObject map = mapper.map();
 
 	// ---------------------------------------------
 	//
