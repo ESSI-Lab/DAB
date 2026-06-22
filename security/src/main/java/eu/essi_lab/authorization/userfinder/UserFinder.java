d74692ff63ec925ae27a7d3187e8a4a0878d1b8d
Enrico Boldrini <enrico.boldrini@cnr.it>
2026-06-22 11:17:39 +0200

    [main] his user registration

--- security/src/main/java/eu/essi_lab/authorization/userfinder/UserFinder.java
+++ security/src/main/java/eu/essi_lab/authorization/userfinder/UserFinder.java
@@ -13,12 +13,12 @@ package eu.essi_lab.authorization.userfinder;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -49,7 +49,7 @@ public class UserFinder {
     /**
      *
      */
-    private static final int REFRESH_INTERVAL_MINUTES = 30;
+    private static final int REFRESH_INTERVAL_MINUTES = 5;
 
     private static SnapshotStore<GSUser> usersStore;
 
